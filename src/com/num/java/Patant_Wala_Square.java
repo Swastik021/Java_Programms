fcb20371435b918811913031934594152e14e027
Swastik021 <vu1s2021021@pvppcoe.ac.in>
2023-08-14 21:07:05 +0530

    Pattern

---
+++ src/com/num/java/Patant_Wala_Square.java
@@ -0,0 +1,29 @@
+package com.num.java;
+
+public class Patant_Wala_Square {
+
+	public static void main(String[] args) {
+int n =7;int st = 1;
+int sp = 3;   
+for(int i = 1;i<=n;i++) {
+	for(int s=1;s<=sp;s++) {
+		System.out.print(" ");
+	}
+	for(int j=1;j<=st;j++) {
+		System.out.print("*");
+	}
+	if(i<=n/2) {
+		st = st +2;
+		sp--;
+	
+	}else {
+		st = st-2;
+		sp++;
+	}
+	System.out.println();
+
+}
+
+	}
+
+}
